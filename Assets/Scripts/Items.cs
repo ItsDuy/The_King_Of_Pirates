bfe9dc0d2dc175d86950c614226806b8c537ae98
Nguyen Anh Duy <duy.nguyen280805@hcmut.edu.vn>
2026-04-14 19:00:53 +0700

    Add new Items

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -12,24 +12,19 @@ public class Items : MonoBehaviour
     {
         if (((1 << collision.gameObject.layer) & playerLayer.value) != 0)
         {
-           Debug.Log("Player collected an item!");
+
         }
         switch(id)
         {
             case 0:
-                Debug.Log("Player collected a potion!");
                 break;
             case 1:
-                Debug.Log("Player collected a coin!");
                 break;
             case 2:
-                Debug.Log("Player collected a blue gem!");
                 break;
             case 3:
-                Debug.Log("Player reached a checkpoint!");
                 break;
             default:
-                Debug.Log("Unknown item collected.");
                 break;
         }
         Destroy(gameObject);
